1cf3c58bbd2893c07722317e9c26b985408345ce
Matt Putnam <matt.s.putnam@gmail.com>
2015-03-22 19:16:14 -0400

    Added dialog options to include logic to run if a button is pressed

--- src/common/swing/dialog/Dialog.java
+++ src/common/swing/dialog/Dialog.java
@@ -14,8 +14,8 @@ public class Dialog {
   
   /**
    * Asks the user to confirm something.
-   * @param parent - the parent component, used for anchoring.  Can be null.
-   * @param message - the message to display
+   * @param parent the parent component, used for anchoring.  Can be null.
+   * @param message the message to display
    * @return <tt>true</tt> if the user confirmed the dialog
    */
   public static boolean confirm(Component parent, Object message) {
@@ -23,10 +23,21 @@ public class Dialog {
   }
   
   /**
+   * Asks the user to confirm something, and runs some logic if confirmed.
+   * @param parent the parent component, used for anchoring.  Can be null.
+   * @param message the message to display
+   * @param onOK the logic to run if confirmed
+   */
+  public static void confirm(Component parent, Object message, Runnable onOK) {
+    if (confirm(parent, message))
+      onOK.run();
+  }
+  
+  /**
    * Asks the user to confirm something.
-   * @param parent - the parent component, used for anchoring.  Can be null.
-   * @param message - the message to display
-   * @param title - the title of the dialog
+   * @param parent the parent component, used for anchoring.  Can be null.
+   * @param message the message to display
+   * @param title the title of the dialog
    * @return <tt>true</tt> if the user confirmed the dialog
    */
   public static boolean confirm(Component parent, Object message, String title) {
@@ -35,10 +46,22 @@ public class Dialog {
   }
   
   /**
+   * Asks the user to confirm something, and runs some logic if confirmed
+   * @param parent the parent component, used for anchoring.  Can be null.
+   * @param message the message to display
+   * @param title the title of the dialog
+   * @param onOK the logic to run if confirmed
+   */
+  public static void confirm(Component parent, Object message, String title, Runnable onOK) {
+    if (confirm(parent, message, title))
+      onOK.run();
+  }
+  
+  /**
    * Asks the user to answer yes or no to a question.
-   * @param parent - the parent component, user for anchoring.  Can be null.
-   * @param message - the message to display
-   * @param title - the title of the dialog
+   * @param parent the parent component, user for anchoring.  Can be null.
+   * @param message the message to display
+   * @param title the title of the dialog
    * @return <tt>true</tt> if the user answered "yes"
    */
   public static boolean askYesNo(Component parent, Object message, String title) {
@@ -47,9 +70,24 @@ public class Dialog {
   }
   
   /**
+   * Asks the user to answer yes or no to a question, and runs some logic either way
+   * @param parent the parent component, user for anchoring.  Can be null.
+   * @param message the message to display
+   * @param title the title of the dialog
+   * @param onYes the logic to run if the 'Yes' button is pressed
+   * @param onNo the logic to run if the 'No' button is pressed
+   */
+  public static void askYesNo(Component parent, Object message, String title, Runnable onYes, Runnable onNo) {
+    if (askYesNo(parent, message, title))
+      onYes.run();
+    else
+      onNo.run();
+  }
+  
+  /**
    * Notifies the user of an error.
-   * @param parent - the parent component, used for anchoring.  Can be null.
-   * @param message - the message to display
+   * @param parent the parent component, used for anchoring.  Can be null.
+   * @param message the message to display
    */
   public static void error(Component parent, Object message) {
     error(parent, message, "Error");
@@ -57,9 +95,9 @@ public class Dialog {
   
   /**
    * Notifies the user of an error.
-   * @param parent - the parent component, used for anchoring.  Can be null.
-   * @param message - the message to diplay
-   * @param title - the title of the dialog
+   * @param parent the parent component, used for anchoring.  Can be null.
+   * @param message the message to diplay
+   * @param title the title of the dialog
    */
   public static void error(Component parent, Object message, String title) {
     JOptionPane.showMessageDialog(parent, message, title, JOptionPane.ERROR_MESSAGE);
@@ -67,8 +105,8 @@ public class Dialog {
   
   /**
    * Displays information to the user.
-   * @param parent - the parent component, used for anchoring.  Can be null.
-   * @param message - the message to display
+   * @param parent the parent component, used for anchoring.  Can be null.
+   * @param message the message to display
    */
   public static void info(Component parent, Object message) {
     info(parent, message, "Info");
@@ -76,9 +114,9 @@ public class Dialog {
   
   /**
    * Displays information to the user.
-   * @param parent - the parent component, used for anchoring.  Can be null.
-   * @param message - the message to display
-   * @param title - the title of the dialog
+   * @param parent the parent component, used for anchoring.  Can be null.
+   * @param message the message to display
+   * @param title the title of the dialog
    */
   public static void info(Component parent, Object message, String title) {
     JOptionPane.showMessageDialog(parent, message, title, JOptionPane.INFORMATION_MESSAGE);
